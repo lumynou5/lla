600e7144dd92e483239dd30ce47bc26e5753d3f0
lumynou5 <lumynou5.tw@gmail.com>
2023-04-24 10:29:01 +0800

    perf: add GCC pure function attribute

--- include/macro.hpp
+++ include/macro.hpp
@@ -2,9 +2,9 @@
 #define LLA_MACRO_HPP
 
 #if __cplusplus >= 202002L
-    #define LLA_NODISCARD_PURE [[nodiscard("pure function")]]
+    #define LLA_NODISCARD_PURE [[nodiscard("pure function")]] [[gnu::pure]]
 #else
-    #define LLA_NODISCARD_PURE [[nodiscard]]
+    #define LLA_NODISCARD_PURE [[nodiscard]] [[gnu::pure]]
 #endif
 
 #endif //LLA_MACRO_HPP
